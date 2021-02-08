53bd35f67afe1a5cb0fb524e6caec76ac3facddc
JimmyDang95 <jimmydang12@hotmail.de>
2021-02-08 20:47:06 +0100

    Added RelationalOperator task

---
+++ src/main/java/RelationalOperatoren.java
@@ -0,0 +1,22 @@
+public class RelationalOperatoren {
+    public static void main(String[] args) {
+
+        int var1 = 5;
+        int var2 = 10;
+
+        boolean resultOfSmallerAs = var1 < var2; //5 ist kleiner als 10 = true
+        System.out.println(resultOfSmallerAs);
+
+        boolean resultOfGreaterAs = var1 > var2; //5 ist größer als 10 = false
+        System.out.println(resultOfGreaterAs);
+
+        boolean resultOfEquality = var1 == var2; // 5 ist gleich 10 = false
+        System.out.println(resultOfEquality);
+
+
+
+
+
+
+    }
+}
