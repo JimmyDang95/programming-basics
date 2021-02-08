36ae1d5121252ecd2678d31d7ba4b316e9b1db78
JimmyDang95 <jimmydang12@hotmail.de>
2021-02-08 20:34:32 +0100

    Added first completed task

--- src/main/java/AufgabeOperatoren.java
+++ src/main/java/AufgabeOperatoren.java
@@ -1,2 +1,11 @@
 public class AufgabeOperatoren {
+    public static void main(String[] args) {
+
+        int var1 = 2;
+        int var2 = 2;
+
+        int result = var1 + var2;
+
+        System.out.println("Das Ergebnis von " + var1 + " und von " + var2 + " ist gleich " +result);
+    }
 }
