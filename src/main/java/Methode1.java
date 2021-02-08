d6569afd4c7c7e9be997a161c4bdd473d4df2e65
JimmyDang95 <jimmydang12@hotmail.de>
2021-02-08 20:55:35 +0100

    Added Methode1 task

---
+++ src/main/java/Methode1.java
@@ -0,0 +1,15 @@
+public class Methode1 {
+    public static void main(String[] args) {
+
+
+        int result = add(1, 2);
+
+
+        System.out.println("Das Ergebnis der Addition lautet " + result);
+
+    }
+
+    public static int add(int var1, int var2){
+        return var1 + var2;
+    }
+}
